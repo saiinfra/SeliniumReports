71dfacad3c599a6b7d838dcd02c8548e6a11bccd
sri595 <sri.cse595@gmail.com>
2015-12-10 12:00:41 +0530

    first commit

---
+++ src/com/ontestautomation/selenium/ci/SampleTest.java
@@ -0,0 +1,8 @@
+package com.ontestautomation.selenium.ci;
+
+public class SampleTest {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/com/ontestautomation/selenium/ci/SeleniumCITest.java
@@ -0,0 +1,62 @@
+package com.ontestautomation.selenium.ci;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumCITest {
+	
+	static WebDriver driver;
+	
+	@Before
+	public void setup() {
+		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);				
+	}
+	
+	@Test
+	public void successfulLoginLogout() {
+		
+		driver.get("http://parabank.parasoft.com");
+		Assert.assertEquals(driver.getTitle(), "ParaBank | Welcome | Online Banking");
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.name("password")).sendKeys("demo");
+		driver.findElement(By.cssSelector("input[value='Log In']")).click();
+		Assert.assertEquals(driver.getTitle(), "ParaBank | Accounts Overview");
+		driver.findElement(By.linkText("Log Out")).click();
+		Assert.assertEquals(driver.getTitle(), "ParaBank | Welcome | Online Banking");
+	}
+
+	@Test
+	public void incorrectLogin() {
+		
+		driver.get("http://parabank.parasoft.com");
+		Assert.assertEquals(driver.getTitle(), "ParaBank | Welcome | Online Banking");
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.name("password")).sendKeys("incorrectpassword");
+		driver.findElement(By.cssSelector("input[value='Log In']")).click();
+		Assert.assertEquals(driver.getTitle(), "ParaBank | Error");
+		String errorText = driver.findElement(By.cssSelector("p[class='error']")).getText();
+		Assert.assertEquals(errorText, "The username and password could not be verified.");
+	}
+	
+	@Test
+	public void thisTestShouldFail() {
+		
+		driver.get("http://parabank.parasoft.com");
+		Assert.assertEquals(driver.getTitle(), "ParaBank | How are you today? | Online Banking");
+	}
+	
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+	
+}
