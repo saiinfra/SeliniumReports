ea41980fbf5e5ee7fe382256e4761083686ab169
sri595 <sri.cse595@gmail.com>
2015-12-17 17:34:57 +0530

    version2

--- src/com/ontestautomation/selenium/ci/TestRunner.java
+++ src/com/ontestautomation/selenium/ci/TestRunner.java
@@ -8,7 +8,7 @@ public class TestRunner {
 	@Test
 	public void test() {
 		File file = new File(
-				"/var/lib/jenkins/jobs/TestFramework2/workspace/report/allclasses-frame.html");
+				"/var/lib/jenkins/jobs/TestFramework2/workspace/repor/allclasses-frame.html");
 		System.out.println("AbsolutePath" +file.getAbsolutePath());
 
 	}
